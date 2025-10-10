40019c4879bba53fa60a82b7d64484a3abd4cc20
praytoo <praytoo52@icloud.com>
2025-10-10 10:42:55 -0700

    update

--- src/main/java/com/pluralsight/TransactionFileManager.java
+++ src/main/java/com/pluralsight/TransactionFileManager.java
@@ -57,9 +57,44 @@ public class TransactionFileManager {
         }
     }
     public static boolean addPayment(){
-        return false;
+        System.out.println("What is the amount of the payment you would like to debit?");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Who is the vendor?");
+        String vendor = scanner.nextLine();
+        System.out.println("What is the description of the payment?");
+        String description = scanner.nextLine();
+        while (amount <= 0) {
+            System.out.println("Invalid amount, please enter a positive number");
+            System.out.println("What is the amount of the payment you would like to debit?");
+            amount = scanner.nextDouble();
+            scanner.nextLine();
+            return false;
+        }
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        date.format(dateFormatter);
+        time.format(timeFormatter);
+
+        String dateString = date.format(dateFormatter);
+        String timeString = time.format(timeFormatter);
+
+        amount = -amount;
+        String line = dateString + "|" + timeString + "|" + description + "|" + vendor + "|" + "$" + amount;
+        line = line + "\n";
+        System.out.println(line);
+
+        Path path = Paths.get("transactions.csv");
+        Charset charset = StandardCharsets.UTF_8;
+        try (BufferedWriter writer = Files.newBufferedWriter(path, charset, StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
+            writer.write(line);
+            System.out.println("Debit successfully recorded!");
+            return true;
+        } catch (IOException e) {
+            System.out.println("Error writing to file." + e.getMessage());
+            return false;
+        }
     }
-
-
-
 }
